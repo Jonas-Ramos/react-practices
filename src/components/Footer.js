d29b170551e1a342fd70fb7e1d055392194bea93
Jony M <jonymonroy@gmail.com>
2021-01-27 20:57:08 -0500

    First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => 
+        <footer className='footer' id='footer'>
+            <p>Todos los derechos reservados &copy;</p>
+        </footer>
+
+ 
+export default Footer;
